c3877a08171be0242debdc70f4c1b68ec1cda3e2
Канат <Kanat032005@gmail.com>
2025-06-06 03:13:34 +0600

    fix img path

--- src/data/cars.tsx
+++ src/data/cars.tsx
@@ -19,7 +19,7 @@ export const cars: Car []= [
         id: 2,
         make: 'Ferrari',
         name: 'Ferrari 488 GTB',
-        img: '../../public/img/Ferrari 488 GTB.webp',
+        img: '/img/Ferrari 488 GTB.webp',
         disc: 'Ferrari 488 GTB — взрывная мощь, итальянский стиль и точность в каждой детали.',
         price: '210 000'
     },
@@ -28,7 +28,7 @@ export const cars: Car []= [
         id: 3,
         make: 'Mercedes-Benz',
         name: 'Mercedes-Benz AMG GT',
-        img: '../../public/img/mercedes benz amg gt.jpg',
+        img: '/img/mercedes benz amg gt.jpg',
         disc: 'Mercedes-AMG GT — роскошь, скорость и характер настоящего гран туризмо.',
         price: '130 000'
     },
@@ -36,7 +36,7 @@ export const cars: Car []= [
         id: 4,
         make: 'Lamborghini',
         name: 'Lamborghini Huracán',
-        img: '../../public/img/lamborghini huracan.jpg',
+        img: '/img/lamborghini huracan.jpg',
         disc: 'Lamborghini Huracán — агрессивный дизайн, ревущий двигатель и драйв без компромиссов.',
         price: '200 000'
     },
@@ -44,7 +44,7 @@ export const cars: Car []= [
         id: 5,
         make: 'Pagani',
         name: 'Pagani Huayra',
-        img: '../../public/img/pagani huayra.jpg',
+        img: '/img/pagani huayra.jpg',
         disc: 'Pagani Huayra — эксклюзивный гиперкар с искусством инженерии и роскошью ручной работы.',
         price: '1 500 000'
     },
@@ -52,7 +52,7 @@ export const cars: Car []= [
         id: 6,
         make: 'Bugatti',
         name: 'Bugatti Chiron',
-        img: '../../public/img/bugatti-chiron.jpg',
+        img: '/img/bugatti-chiron.jpg',
         disc: 'Bugatti Chiron — абсолютный рекорд скорости и инженерного совершенства.',
         price: '3 000 000'
     },
@@ -60,7 +60,7 @@ export const cars: Car []= [
         id: 7,
         make: 'McLaren',
         name: 'McLaren 720S',
-        img: '../../public/img/mclaren.webp',
+        img: '/img/mclaren.webp',
         disc: 'McLaren 720S — ультралёгкий суперкар с невероятной аэродинамикой и мощностью.',
         price: '300 000'
     },
@@ -68,7 +68,7 @@ export const cars: Car []= [
         id: 8,
         make: 'Bentley',
         name: 'Bentley Continental GT',
-        img: '../../public/img/bentley continental.avif',
+        img: '/img/bentley continental.avif',
         disc: 'Bentley Continental GT — сочетание безупречной роскоши и мощного двигателя.',
         price: '220 000'
     },
@@ -76,7 +76,7 @@ export const cars: Car []= [
         id: 12,
         make: 'Subaru',
         name: 'Subaru WRX STI',
-        img: '../../public/img/subaru wrx sti.jpg',
+        img: '/img/subaru wrx sti.jpg',
         disc: 'Subaru WRX STI — легендарный раллийный спорткар с ярким характером и полный привод.',
         price: '40 000'
     }
